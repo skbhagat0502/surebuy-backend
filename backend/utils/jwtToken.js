460690f7df60219f42e0843848f8bea22fdee851
skbhagat0502 <sandeepbh0502@gmail.com>
2023-12-23 19:25:42 +0530

    token issue resolved

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -7,6 +7,8 @@ const sendToken = (user, statusCode, res) => {
     httpOnly: true,
     secure: true,
     sameSite: "none",
+    path: "/",
+    domain: ORIGIN,
   };
 
   res.status(statusCode).cookie("token", token, options).json({
