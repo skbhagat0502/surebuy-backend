44118c122448aa828489f48161c533d5321710ec
skbhagat0502 <sandeepbh0502@gmail.com>
2023-12-23 14:41:51 +0530

    done done

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -4,9 +4,9 @@ const sendToken = (user, statusCode, res) => {
   const token = user.getJWTToken();
   const options = {
     expires: new Date(Date.now() + COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-    httpOnly: true,
-    secure: false,
-    sameSite: "lax",
+    // httpOnly: true,
+    secure: true,
+    sameSite: "none",
     path: "/",
     domain: ORIGIN,
   };
