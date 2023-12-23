f203fd86f1251edfd60869caaecbded5dd083017
skbhagat0502 <sandeepbh0502@gmail.com>
2023-12-23 13:52:48 +0530

    credential

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,8 +1,7 @@
 // Create Token and saving in cookie
-import { COOKIE_EXPIRE, NODE_ENV, ORIGIN } from "../constants.js";
+import { COOKIE_EXPIRE, ORIGIN } from "../constants.js";
 const sendToken = (user, statusCode, res) => {
   const token = user.getJWTToken();
-  // options for cookie
   const options = {
     expires: new Date(Date.now() + COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
     httpOnly: true,
